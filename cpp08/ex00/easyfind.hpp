72079926fbfb68ff4de08a1aec851fe0c7158697
Ingmar Kole <ikole@student.codam.nl>
2021-05-06 10:25:29 +0200

    Slightly improved my variable name hehe

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -8,10 +8,10 @@
 template<typename T>
 typename T::iterator easyfind(T arr, int toFind)
 {
-	typename T::iterator fuck = std::find(arr.begin(), arr.end(), toFind);
-	if (fuck == arr.end())
+	typename T::iterator itr = std::find(arr.begin(), arr.end(), toFind);
+	if (itr == arr.end())
 		throw std::exception();
-	return fuck;
+	return itr;
 }
 
 #endif
