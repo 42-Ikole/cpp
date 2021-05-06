d571040af1450f98a37f7629598571c180da39ed
Ingmar Kole <ikole@student.codam.nl>
2021-05-06 17:05:25 +0200

    wat een kanker opdracht ik haat dit met heel mn leven krijg de tering

---
+++ cpp08/ex02/mutantstack.hpp
@@ -0,0 +1,55 @@
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <deque>
+
+template <class T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+	public:
+		MutantStack() {}
+		MutantStack(const MutantStack &tbc) {*this = tbc;}
+		MutantStack & operator = (const MutantStack &tba) {*this = tba; return *this;}
+		virtual ~MutantStack() {}
+
+		typedef typename Container::iterator				iterator;
+		typedef typename Container::const_iterator			const_iterator;
+		typedef typename Container::reverse_iterator		reverse_iterator;
+		typedef typename Container::const_reverse_iterator	const_reverse_iterator;
+
+		iterator begin() {
+			return this->c.begin();
+		}
+
+		const_iterator begin() const {
+			return this->c.begin();
+		}
+
+		reverse_iterator rbegin() {
+			return this->c.rbegin();
+		}
+
+		const_reverse_iterator rbegin() const {
+			return this->c.rbegin();
+		}
+
+		iterator end() {
+			return this->c.end();
+		}
+
+		const_iterator end() const {
+			return this->c.end();
+		}
+
+		reverse_iterator rend() {
+			return this->c.rend();
+		}
+
+		const_reverse_iterator rend() const {
+			return this->c.rend();
+		}
+};
+
+#endif
