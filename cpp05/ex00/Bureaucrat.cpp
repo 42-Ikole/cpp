89b051797c7847b4be110ea161ce5b93fb0757d8
Ingmar Kole <ikole@student.codam.nl>
2022-03-02 17:30:35 +0100

    Fix: rewrote day5 ex00

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,84 @@
+
+#include <Bureaucrat.hpp>
+#include <string>
+#include <ostream>
+
+/////////////
+// Coplien //
+/////////////
+
+	Bureaucrat::Bureaucrat(const std::string name, short grade)
+		: _name(name)
+	{
+		_gradeInRange(grade);
+		this->_grade = grade;
+	}
+
+	Bureaucrat::Bureaucrat(const Bureaucrat& x)
+	{
+		*this = x;	
+	}
+
+	Bureaucrat&	Bureaucrat::operator = (const Bureaucrat& x)
+	{
+		if (this == &x)
+			return *(this);
+
+		this->_grade = x._grade;
+		return *(this);
+	}
+
+	Bureaucrat::~Bureaucrat()
+	{}
+
+/////////////
+// Helpers //
+/////////////
+
+	void	Bureaucrat::_gradeInRange(short	grade)
+	{
+		if (grade <= 0)
+			throw Bureaucrat::GradeTooHighException();
+		else if (grade > 150)
+			throw Bureaucrat::GradeTooLowException();
+	}
+
+/////////////
+// Getters //
+/////////////
+
+	const std::string&	Bureaucrat::getName(void) const
+	{
+		return (_name);
+	}
+
+	short				Bureaucrat::getGrade(void) const
+	{
+		return (_grade);
+	}
+
+///////////////
+// Modifiers //
+//////////////
+
+	void	Bureaucrat::incrementGrade(unsigned short n)
+	{
+		_gradeInRange(_grade - n);
+		_grade -= n;
+	}
+
+	void	Bureaucrat::decrementGrade(unsigned short n)
+	{
+		_gradeInRange(_grade + n);
+		_grade += n;
+	}
+
+////////////////////////
+// Operator overloads //
+////////////////////////
+
+	std::ostream&	operator << (std::ostream& o, Bureaucrat& x)
+	{
+		o << x.getName() << ", grade: " << x.getGrade();
+		return (o);
+	}
---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,105 @@
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <exception>
+# include <iostream>
+
+///////////////////
+// COLOR DEFINES //
+///////////////////
+
+	# define COLOR_RED		"\033[31m"
+	# define COLOR_GREEN	"\033[32m"
+	# define COLOR_YELLOW	"\033[33m"
+
+	# define COLOR_LBLUE 	"\033[38;5;51m"
+	# define COLOR_ORANGE	"\033[38;5;214m"
+	# define COLOR_PINK		"\033[38;5;205m"
+
+	# define COLOR_RESET	"\033[0m"
+
+//////////////
+// Messages //
+//////////////
+
+	# define ERROR		COLOR_RED "ERROR: " COLOR_RESET
+	# define CREATED	COLOR_GREEN "Created: " COLOR_RESET
+	# define INCR		COLOR_YELLOW "Incremented: " COLOR_RESET
+	# define DECR		COLOR_ORANGE "Decremented: " COLOR_RESET
+	# define ASSIGN		COLOR_LBLUE	"Assigned: " COLOR_RESET
+
+class Bureaucrat
+{
+	private:
+
+		const std::string	_name;
+		short 				_grade;
+
+	//////////////////
+	// Constructors //
+	//////////////////
+	private:
+
+		Bureaucrat();
+
+	public:
+
+		Bureaucrat(const std::string name, short grade);
+		Bureaucrat(const Bureaucrat& x);
+		virtual ~Bureaucrat();
+
+		Bureaucrat& operator = (const Bureaucrat& x);
+	
+	/////////////
+	// Helpers //
+	/////////////
+	private:
+
+		void	_gradeInRange(short	grade);
+
+	/////////////
+	// Getters //
+	/////////////
+	public:
+
+		const std::string&	getName(void) const;
+		short				getGrade(void) const;
+
+	///////////////
+	// Modifiers //
+	///////////////
+	public:
+
+		void	incrementGrade(unsigned short n = 1);
+		void	decrementGrade(unsigned short n = 1);
+
+	////////////////
+	// Exceptions //
+	////////////////
+	public:
+
+		class GradeTooHighException : public std::exception
+		{
+			const char* what() const throw() {
+				return ("grade too high");
+			}
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			const char* what() const throw() {
+				return ("grade too low");
+			}
+		};
+
+}; /* Bureaucrat */
+
+////////////////////////
+// Operator overloads //
+////////////////////////
+
+	std::ostream& operator << (std::ostream& o, Bureaucrat& x);
+
+#endif
