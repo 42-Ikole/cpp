10be5cb3ac2c9b7481ca30dd01aef29fc2d33d39
Ingmar Kole <ikole@student.codam.nl>
2022-03-13 14:36:50 +0100

    Feat: tests

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -1,9 +1,10 @@
 
 #include <Bureaucrat.hpp>
+#include <Form.hpp>
 #include <string>
 #include <iostream>
 
-void	construction(void)
+static void	bureaucrat_constructors(void)
 {
 	// should work fine
 	try {
@@ -26,8 +27,8 @@ void	construction(void)
 		std::cout << ASSIGN << assign << "\n";
 
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
 
 	// should throw exception
@@ -36,8 +37,8 @@ void	construction(void)
 		Bureaucrat snorlax("Snorlax", 0);
 		std::cout << CREATED << snorlax << "\n";
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
 
 	try {
@@ -45,8 +46,8 @@ void	construction(void)
 		Bureaucrat snorlax("Snorlax", 151);
 		std::cout << CREATED << snorlax << "\n";
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
 
 	try {
@@ -54,8 +55,8 @@ void	construction(void)
 		Bureaucrat snorlax("Snorlax", -150);
 		std::cout << CREATED << snorlax << "\n";
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
 
 	try {
@@ -63,12 +64,56 @@ void	construction(void)
 		Bureaucrat snorlax("Snorlax", 420);
 		std::cout << CREATED << snorlax << "\n";
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
 }
 
-static void	modifiers(void)
+static void form_constructors(void)
+{
+	try {
+		Form n1("de nummer 1", 69, 2);
+		std::cout << CREATED << n1 << "\n";
+
+		Form copy(n1);
+		std::cout << CREATED << copy << "\n";
+
+		Form assignation = copy;
+		std::cout << CREATED << assignation << "\n";
+	} catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
+	}
+
+	// too high
+	try {
+		Form high("too high", -420, 100);
+	} catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
+	}
+	
+	// too high
+	try {
+		Form high("too high", 100, -420);
+	} catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
+	}
+
+	// too low
+	try {
+		Form high("too low", 420, 100);
+	} catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
+	}
+	
+	// too low
+	try {
+		Form high("too low", 100, 420);
+	} catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
+	}
+}
+
+static void	bureaucrat_modifiers(void)
 {
 	// should not throw
 	try
@@ -82,21 +127,21 @@ static void	modifiers(void)
 		snorlax.decrementGrade(69);
 		std::cout << DECR << snorlax << "\n";
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
 
 	// should throw
 	try {
-		Bureaucrat snorlax("Snorlax", 69);
-		std::cout << CREATED << snorlax << "\n";
+		Bureaucrat high("high", 69);
+		std::cout << CREATED << high << "\n";
 		
 		// too high
-		snorlax.incrementGrade(69);
-		std::cout << INCR << snorlax << "\n";
+		high.incrementGrade(69);
+		std::cout << INCR << high << "\n";
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
 
 	try {
@@ -107,9 +152,35 @@ static void	modifiers(void)
 		snorlax.decrementGrade(420);
 		std::cout << INCR << snorlax << "\n";
 	}
-	catch (std::exception& e) {
-		std::cout << ERROR << e.what() << "\n";
+	catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
 	}
+}
+
+static void form_modifiers(void)
+{
+	Form woop("woop", 42, 16);
+	std::cout << CREATED << woop << "\n";
+
+	Bureaucrat bob("Bob", 43);
+	std::cout << CREATED << bob << "\n";
+
+	try {
+		// grade too low
+		bob.signForm(woop);
+
+		bob.incrementGrade();
+
+		// signs form
+		bob.signForm(woop);
+
+		// already signed
+		bob.signForm(woop);
+
+	} catch (const std::exception& e) {
+		std::cerr << ERROR << e.what() << "\n";
+	}
+
 }
 
 static void run_test(const char* name, void (*test)())
@@ -120,7 +191,9 @@ static void run_test(const char* name, void (*test)())
 
 int main(void)
 {
-	run_test("Constructors", construction);
-	run_test("Modifiers", modifiers);
+	run_test("Bureaucrat Constructors", bureaucrat_constructors);
+	run_test("Form Constructors", form_constructors);
+	run_test("Bureaucrat Modifiers", bureaucrat_modifiers);
+	run_test("Form modifiers", form_modifiers);
 	return (0);
 }
